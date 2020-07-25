6cce30bd247289140c7aeb592aef98af87fea1fd
Marc Carré <carre.marc@gmail.com>
2020-07-25 19:05:20 +0900

    Have current directory as constant

--- tests/contests/practice/test_a.py
+++ tests/contests/practice/test_a.py
@@ -5,6 +5,9 @@ import pytest
 from contests.practice.a import main
 
 
+DIR = abspath(dirname(__file__))
+
+
 def test_a_01(capsys, monkeypatch):
     expected_out = set_up(monkeypatch)
     main()
@@ -21,10 +24,9 @@ def test_a_02(capsys, monkeypatch):
 
 def set_up(monkeypatch):
     test_name = inspect.stack()[1][3].replace('test_', '')
-    directory = abspath(dirname(__file__))
-    with open(join(directory, '%s_in.txt' % test_name), 'r') as f:
+    with open(join(DIR, '%s_in.txt' % test_name), 'r') as f:
         monkeypatch.setattr('sys.stdin', StringIO(f.read()))
-    with open(join(directory, '%s_out.txt' % test_name), 'r') as f:
+    with open(join(DIR, '%s_out.txt' % test_name), 'r') as f:
         return f.read().strip()
 
 
